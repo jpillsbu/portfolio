81a32693403a2c4a8c3045f1d64103aa53bf2862
John Pillsbury <jpillsbu@gmail.com>
2023-05-18 22:19:12 -0400

    Update about page with linting

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 
 const About = () => {
+    const essay = [
+        "John Pillsbury's portfolio and blog website is a platform where he showcases his learnings and projects to the world. As a passionate learner and a tech enthusiast, John loves to explore various technologies and tools and build things that solve real-world problems. His portfolio website is a perfect reflection of his skills and creativity, and it serves as a testament to his commitment to continuous learning.",
+        "On John's portfolio website, visitors can explore his various projects and get a glimpse of the different technologies he has worked with. John's website also includes a blog section where he shares his insights, thoughts, and experiences about the tech industry. Visitors can learn about new tools, techniques, and technologies that John has explored and find inspiration for their own projects.",
+        "John's portfolio and blog website is not just a showcase of his skills, but it also serves as a valuable resource for anyone interested in technology and programming. John is committed to sharing his knowledge and experience with others and hopes that his website will inspire and motivate others to pursue their own passions and build great things. Whether you are a seasoned developer or just starting in the tech industry, John's website has something to offer for everyone."
+    ]
+
     return (
         <div className="h-[calc(100vh-4rem)]">
             <div className="h-full">
@@ -9,13 +15,11 @@ const About = () => {
                 </div>
                 <div className="h-[calc(100vh-9rem)] flex justify-between">
                     <div className="w-full flex flex-col justify-center">
-                        <p className="p-6 text-2xl">John Pillsbury's portfolio and blog website is a platform where he showcases his learnings and projects to the world. As a passionate learner and a tech enthusiast, John loves to explore various technologies and tools and build things that solve real-world problems. His portfolio website is a perfect reflection of his skills and creativity, and it serves as a testament to his commitment to continuous learning.
-                        </p>
-                        <p className="p-6 text-2xl">
-                            On John's portfolio website, visitors can explore his various projects and get a glimpse of the different technologies he has worked with. John's website also includes a blog section where he shares his insights, thoughts, and experiences about the tech industry. Visitors can learn about new tools, techniques, and technologies that John has explored and find inspiration for their own projects.
-                        </p>
-                        <p className="p-6 text-2xl">
-                            John's portfolio and blog website is not just a showcase of his skills, but it also serves as a valuable resource for anyone interested in technology and programming. John is committed to sharing his knowledge and experience with others and hopes that his website will inspire and motivate others to pursue their own passions and build great things. Whether you are a seasoned developer or just starting in the tech industry, John's website has something to offer for everyone.</p>
+                        {essay.map((paragraph) => {
+                            return (
+                                <p className="p-6 text-2xl" key={paragraph}>{paragraph}</p>
+                            )
+                        })}
                     </div>
                     <div className="w-full flex justify-center items-center">
                         <Image className="h-4/5 object-contain rounded-3xl" src="profile.png" alt="profile picture" />
