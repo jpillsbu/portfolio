3cd70a2edbbd47397ca6903b6b19794dbf7a54be
John Pillsbury <jpillsbu@gmail.com>
2023-05-23 22:36:46 -0400

    Fix navbar mobile features

--- app/header.tsx
+++ app/header.tsx
@@ -1,39 +1,58 @@
+"use client";
+
 import Link from "next/link"
+import { useState } from "react"
 
 function Header() {
+    const [navbar, setNavbar] = useState(false)
     return (
         <header>
-            <nav className="bg-dark border-gray-200 dark:bg-white">
-                <div className="max-w-screen-3xl flex flex-wrap items-center justify-between mx-auto p-4">
-                    <div className="font-semibold text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-black md:p-0 dark:text-black md:dark:hover:text-black dark:hover:bg-white dark:hover:text-black md:dark:hover:bg-transparent">
-                        <Link href="/">
-                            Home
-                        </Link>
-                    </div>
-                    <button data-collapse-toggle="navbar" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar" aria-expanded="false">
-                        <span className="sr-only">Open main menu</span>
-                        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-                    </button>
-                    <div className="hidden w-full md:block md:w-auto" id="navbar">
-                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-white">
-                            <li className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-700 md:p-0 dark:text-black md:dark:hover:text-gray-500 dark:hover:bg-gray-700 dark:hover:text-black md:dark:hover:bg-transparent">
-                                <Link href="/about">
-                                    About
-                                </Link>
-                            </li>
-                            <li className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-700 md:p-0 dark:text-black md:dark:hover:text-gray-500 dark:hover:bg-gray-700 dark:hover:text-black md:dark:hover:bg-transparent">
-                                <Link href="/projects">
-                                    Projects
-                                </Link>
-                            </li>
-                            <li className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-700 md:p-0 dark:text-black md:dark:hover:text-gray-500 dark:hover:bg-gray-700 dark:hover:text-black md:dark:hover:bg-transparent">
-                                <Link href="/blog">
-                                    Blog
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
+            <nav className="bg-gray-50">
+                <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                    <div className={`relative flex ${navbar ? "h-50" : "h-16"} md:h-16 items-center justify-between`}>
+                        <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                            <div className="md:hidden">
+                                <button type="button" className="inline-flex bg-black items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" onClick={() => setNavbar(!navbar)}>
+                                    {navbar ? (
+                                        <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                        </svg>
+                                    ) : (
+                                        <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                                        </svg>
+                                    )}
+                                </button>
+                            </div>
+                        </div>
 
+                        <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? 'pl-4 block' : 'hidden'}`}>
+                            <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                                <li>
+                                    <Link className="md:bg-gray-200 rounded-md text-gray-700 px-3 py-2 text-sm font-medium" onClick={() => setNavbar(false)} href="/">
+                                        Home
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link className="text-gray-700 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" onClick={() => setNavbar(false)} href="/about">
+                                        About
+                                    </Link>
+                                </li>
+                                <li>
+
+                                    <Link className="text-gray-700 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" onClick={() => setNavbar(false)} href="/projects">
+                                        Projects
+                                    </Link>
+                                </li>
+                                <li>
+
+                                    <Link className="text-gray-700 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" onClick={() => setNavbar(false)} href="/blog">
+                                        Blog
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </nav>
         </header>
